00a3cb9fe85ff8f625d2da665c129b43ad58c260
vladimirshkarin <vladimir.shkarin@gmail.com>
2024-08-22 17:25:04 +0300

    Add new solution

--- hexlet_exercises/trees/trees_modify_virtual_fs.py
+++ hexlet_exercises/trees/trees_modify_virtual_fs.py
@@ -1,3 +1,7 @@
+# url: https://ru.hexlet.io/courses/python-trees/lessons/
+# manipulations/exercise_unit
+#
+#
 # Реализуйте функцию compress_images(). Она должна принимать на вход
 # директорию, находить внутри нее картинки и уменьшать
 # свойство size в их метаданных в два
@@ -33,5 +37,39 @@ from hexlet.fs import get_children, get_meta, get_name, is_file, mkdir, mkfile
 
 
 # BEGIN (write your solution here)
+def change_meta(node):
+    name = get_name(node)
+    new_meta = copy.deepcopy(get_meta(node))
+    if name.endswith(".jpg") and isinstance(new_meta.get("size"), (int, float)):
+        new_meta["size"] = new_meta["size"] // 2
+    if is_file(node):
+        return mkfile(name, new_meta)
+    return mkdir(name, get_children(node), new_meta)
 
+
+def compress_images(tree):
+    children = get_children(tree)
+    new_children = list(map(change_meta, children))
+    new_meta = copy.deepcopy(get_meta(tree))
+    new_tree = mkdir(get_name(tree), new_children, new_meta)
+    return new_tree
 # END
+
+
+# BEGIN reference solution
+def compress_images_ref(tree):
+    children = get_children(tree)
+
+    def reduce_image_size(node):
+        name = get_name(node)
+        if not is_file(node) or not name.endswith(".jpg"):
+            return node
+        meta = get_meta(node)
+        new_meta = copy.deepcopy(meta)
+        new_meta["size"] //= 2
+        return mkfile(name, new_meta)
+
+    new_children = map(reduce_image_size, children)
+    new_meta = copy.deepcopy(get_meta(tree))
+    return mkdir(get_name(tree), list(new_children), new_meta)
+# END reference solution

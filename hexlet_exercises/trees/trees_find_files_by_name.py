08106c1cc41690c455b8329ff7a7f3c3f8124b54
vladimirshkarin <vladimir.shkarin@gmail.com>
2024-09-03 12:41:31 +0300

    Add new solution

--- hexlet_exercises/trees/trees_find_files_by_name.py
+++ hexlet_exercises/trees/trees_find_files_by_name.py
@@ -27,7 +27,9 @@
 # # ['/etc/nginx/nginx.conf', '/etc/consul/config.json']
 # Подсказки
 # Для реализации этой логики вам понадобится аккумулятор, в котором будет
-# храниться путь от корня до текущего узла. При проваливании внутрь директорий к нему добавляется имя текущей директории. В остальном логика работы идентична примеру из теории
+# храниться путь от корня до текущего узла. При проваливании внутрь директорий
+# к нему добавляется имя текущей директории. В остальном логика работы
+# идентична примеру из теории
 # Переменную с путем от корня до текущего узла можно назвать ancestry
 # Для построения путей используйте функцию os.path.join()
 
@@ -37,10 +39,30 @@ from hexlet.fs import flatten, get_children, get_name, is_file
 
 
 # BEGIN (write your solution here)
+def find_files_by_name(tree, substring):
+    def walk(node, ancestry):
+        name = get_name(node)
+        path = os.path.join(ancestry, name)
+        if is_file(node) and substring in name:
+            return [path]
+        children = get_children(node)
+        result = list(map(lambda child: walk(child, path), children))
+        return flatten(result)
 
+    return walk(tree, "")
 # END
 
 
 # BEGIN reference solution
+def find_files_by_name_ref(tree, substring):
+    def walk(node, ancestry):
+        name = get_name(node)
+        new_ancestry = os.path.join(ancestry, name)
+        if is_file(node):
+            return [] if name.find(substring) < 0 else new_ancestry
+        children = get_children(node)
+        paths = map(lambda child: walk(child, new_ancestry), children)
+        return flatten(paths)
 
+    return walk(tree, "")
 # END reference solution

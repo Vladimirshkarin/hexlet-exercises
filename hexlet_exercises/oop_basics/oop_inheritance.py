93170ffeb835e160b29419b7d1602ad848ef86f4
vladimirshkarin <vladimir.shkarin@gmail.com>
2024-09-07 12:47:43 +0300

    Add new solution

--- hexlet_exercises/oop_basics/oop_inheritance.py
+++ hexlet_exercises/oop_basics/oop_inheritance.py
@@ -43,10 +43,51 @@ class Counter(object):
 
 
 # BEGIN (write your solution here)
+class LimitedCounter(Counter):
 
+    def __init__(self, limit=1):
+        super().__init__()
+        self.limit = limit
+
+    def inc(self, amount=1):
+        self.value = min(max(self.value + amount, 0), self.limit)
+
+    def dec(self, amount=1):
+        super().dec(amount)
 # END
 
 
 # BEGIN reference solution
+# Решение основано на замене атрибута value на свойство,
+# setter которого ограничивает значение счетчика.
+# Такой подход позволяет сохранить свойства класса даже
+# если пользователь будет менять значение счетчика через
+# присваивание напрямую атрибуту value.
+#
+# Если вы просто унаследуете Counter и переопределите
+# метод inc, то такое решение тоже будет правильным.
+# Данное решение нарочно демонстрирует альтернативный подход :)
+class LimitedCounter(Counter):
+    """A counter with limited maximal value."""
 
+    def __init__(self, limit):
+        """Initialize a new counter with some limit."""
+        self.limit = limit
+        # Свойство должно где-то хранить фактическое значение
+        # счетчика, для чего вводится "скрытый" ("приватный")
+        # атрибут _actual_value:
+        self._actual_value = 0
+        # Инициализация методом родителя делается в конце,
+        # потому что предок уже в __init__ присваивает атрибуту
+        # value значение 0. А это значит, что будет вызван setter,
+        # который использует атрибуты limit и _actual_value.
+        super().__init__()
+
+    @property
+    def value(self):
+        return self._actual_value
+
+    @value.setter
+    def value(self, new_value):
+        self._actual_value = min(max(new_value, 0), self.limit)
 # END reference solution

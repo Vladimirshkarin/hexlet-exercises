831a28469837578bdc44b65a3949321e26e2db20
vladimirshkarin <vladimir.shkarin@gmail.com>
2024-09-07 15:50:53 +0300

    Add new solution, fix tests

--- hexlet_exercises/oop_basics/oop_exceptions.py
+++ hexlet_exercises/oop_basics/oop_exceptions.py
@@ -17,3 +17,38 @@
 
 # get_item(7, "foo") is None  # True
 # get_item('a', {}) is None  # True
+
+from functools import wraps
+
+
+# BEGIN (write your solution here)
+def suppress(types_of_error, or_return=None):
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except types_of_error:
+                return or_return
+
+        return inner
+
+    return wrapper
+# END
+
+
+# BEGIN reference solution
+def suppress_(exception, *, or_return=None):
+    """Suppress exceptions of provided class(es) and return a value instead."""
+
+    def wrapper(function):
+        @wraps(function)
+        def inner(*args, **kwargs):
+            try:
+                return function(*args, **kwargs)
+            except exception:
+                return or_return
+
+        return inner
+
+    return wrapper
+# END reference solution

4cecb56b1e37af7a8cf43a1b75cf72db679ea63c
vladimirshkarin <vladimir.shkarin@gmail.com>
2024-09-07 16:33:12 +0300

    Add solution

--- hexlet_exercises/oop_design/patterns.py
+++ hexlet_exercises/oop_design/patterns.py
@@ -7,5 +7,18 @@ class Klass:
 
 
 # BEGIN (write your solution here)
-
+def to_Klass(dictionary):
+    instance = Klass()
+    for key, value in dictionary.items():
+        setattr(instance, key, value)
+    return instance
 # END
+
+
+# BEGIN reference solution
+def to_Klass_ref(data):
+    klass = Klass()
+    for key, value in data.items():
+        setattr(klass, key, value)
+    return klass
+# END reference solution
